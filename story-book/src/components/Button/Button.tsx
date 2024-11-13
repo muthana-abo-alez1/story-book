a7645fc81582ef17ada9c661a0ec633b1b75859a
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-11-13 21:55:01 +0200

    button

--- story-book/src/components/Button/Button.tsx
+++ story-book/src/components/Button/Button.tsx
@@ -5,14 +5,12 @@ interface ButtonProps {
   variant: "primary" | "secondary" | "text";
   size: "small" | "medium" | "large";
   color: "blue" | "red" | "green";
-  text:string;
+  text: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ variant, size, color,text }) => {
+const Button: React.FC<ButtonProps> = ({ variant, size, color, text }) => {
   return (
-    <button className={`button ${variant} ${size} ${color}`}>
-      {text}
-    </button>
+    <button className={`button ${variant} ${size} ${color}`}>{text}</button>
   );
 };
 
