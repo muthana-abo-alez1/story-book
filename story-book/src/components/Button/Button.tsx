98fb5e2b0ff9a8ee1492f607b2c3b7cbbda4aedb
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-11-13 21:31:00 +0200

    button story book

---
+++ story-book/src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Button.scss";
+
+interface ButtonProps {
+  variant: "primary" | "secondary" | "text";
+  size: "small" | "medium" | "large";
+  color: "blue" | "red" | "green";
+  text:string;
+}
+
+const Button: React.FC<ButtonProps> = ({ variant, size, color,text }) => {
+  return (
+    <button className={`button ${variant} ${size} ${color}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
